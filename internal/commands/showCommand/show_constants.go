b2128367aacbbbdca9aa3d0ec9c24826b75661aa
redjax <5534031+redjax@users.noreply.github.com>
2025-07-14 17:02:59 -0400

    fix(constants): Update show constants method

--- internal/commands/showCommand/show_constants.go
+++ internal/commands/showCommand/show_constants.go
@@ -14,10 +14,19 @@ func NewConstantsCmd() *cobra.Command {
 		Short: "Show platform constants",
 		Run: func(cmd *cobra.Command, args []string) {
 			consts := constants.GetPlatformConstants()
+			fmt.Printf("Uptime: %s\n", consts.Uptime)
+			fmt.Printf("Hostname: %s\n", consts.Hostname)
+			fmt.Printf("Default Shell: %s\n", consts.DefaultShell)
+			fmt.Printf("$HOME path: %s\n", consts.HomeDir)
 			fmt.Printf("Family:         %s\n", consts.Family)
 			fmt.Printf("Distribution:   %s\n", consts.Distribution)
 			fmt.Printf("Release:        %s\n", consts.Release)
 			fmt.Printf("Package Manager: %s\n", consts.PackageManager)
+			fmt.Printf("CPU Architecture: %s\n", consts.Architecture)
+			fmt.Printf("CPU Model: %s\n", consts.CPUModel)
+			fmt.Printf("CPU Count: %d\n", consts.CPUCount)
+			fmt.Printf("TotalRAM (bytes): %d\n", consts.TotalRAM)
+			fmt.Printf("Filesystem: %s\n", consts.Filesystem)
 		},
 	}
 }
