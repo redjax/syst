77e3908946c94753c45fed7f162bd39a20ba990e
redjax <5534031+redjax@users.noreply.github.com>
2025-07-20 00:35:50 -0400

    fix(commits): Add commits to repo info.
    Fix printing git info

--- internal/commands/gitCommand/git_info_cmd.go
+++ internal/commands/gitCommand/git_info_cmd.go
@@ -1,8 +1,6 @@
 package gitcommand
 
 import (
-	"fmt"
-
 	gitservice "github.com/redjax/syst/internal/services/gitService"
 	"github.com/spf13/cobra"
 )
@@ -12,29 +10,7 @@ func NewGitInfoCommand() *cobra.Command {
 		Use:   "info",
 		Short: "Show information about the current Git repository",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			info, err := gitservice.GetRepoInfo()
-			if err != nil {
-				return err
-			}
-
-			fmt.Printf("Repository Path:   %s\n", info.Path)
-			fmt.Printf("Is Git Repo:       %v\n", info.IsRepo)
-
-			if info.IsRepo {
-				fmt.Printf("Current Branch:    %s\n", info.CurrentBranch)
-				fmt.Printf("Repo Size (bytes): %d\n", info.SizeBytes)
-			}
-
-			if len(info.Remotes) == 0 {
-				fmt.Println("No remotes found")
-			} else {
-				fmt.Println("Remotes:")
-				for _, remote := range info.Remotes {
-					fmt.Printf("  +%s:\n", remote.Name)
-					fmt.Printf("      - Fetch: %s\n", remote.FetchURL)
-					fmt.Printf("      - Push:  %s\n", remote.PushURL)
-				}
-			}
+			gitservice.PrintRepoInfo()
 
 			return nil
 		},
