f29941ad76654552a977896c6779e7d2f03aee3e
redjax <5534031+redjax@users.noreply.github.com>
2025-07-21 00:08:50 -0400

    fix(version): Add version check (no download, only check)

--- internal/commands/selfCommand/upgrade.go
+++ internal/commands/selfCommand/upgrade.go
@@ -6,13 +6,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// When --check is passed, don't do an upgrade, just check if one is available
+var checkOnly bool
+
 // NewUpgradeCommand creates the 'self upgrade' command
 func NewUpgradeCommand() *cobra.Command {
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "upgrade",
 		Short: "Upgrade syst CLI to the latest release",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return version.UpgradeSelf(cmd, args)
+			return version.UpgradeSelf(cmd, args, checkOnly)
 		},
 	}
+
+	// Register flags
+	cmd.Flags().BoolVar(&checkOnly, "check", false, "Only check for latest version, don't upgrade if one is found.")
+
+	return cmd
 }
