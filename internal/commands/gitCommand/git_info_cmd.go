36a1691f5fb3e883e6f38a1fc8e17dfd45812814
redjax <no@none.com>
2025-09-02 21:14:09 -0400

    fix(info): Add git info subcommand

--- internal/commands/gitCommand/git_info_cmd.go
+++ internal/commands/gitCommand/git_info_cmd.go
@@ -1,18 +1,17 @@
 package gitcommand
 
 import (
-	gitservice "github.com/redjax/syst/internal/services/gitService"
+	"github.com/redjax/syst/internal/services/gitService/infoService"
 	"github.com/spf13/cobra"
 )
 
 func NewGitInfoCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "info",
-		Short: "Show information about the current Git repository",
+		Short: "Interactive TUI for exploring repository statistics",
+		Long:  "Launch an interactive terminal user interface for exploring detailed repository information including remotes, branches, contributors, and more.",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			gitservice.PrintRepoInfo()
-
-			return nil
+			return infoService.RunRepoInfoTUI()
 		},
 	}
 
